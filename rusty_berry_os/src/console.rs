2167f45f11461ece222ed278d8d9eccdad165f95
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-04-02 09:02:48 +0200

    Better implementation for print

--- rusty_berry_os/src/console.rs
+++ rusty_berry_os/src/console.rs
@@ -5,13 +5,25 @@ use crate::bsp;
 /// Console interface.
 
 pub mod interface {
-    pub use core::fmt::Write;
+    pub use core::fmt;
+
+    pub trait Write {
+        fn write_fmt(&self, args: fmt::Arguments) -> core::fmt::Result;
+    }
+
+    pub trait Statistics {
+        fn chars_written(&self) -> usize {
+            0
+        }
+    }
+
+    pub trait All: Write + Statistics {}
 }
 
 /// Return a reference to the console.
 ///
 /// Global instance used by all println macros.
 
-pub fn console() -> impl interface::Write {
+pub fn console() -> &'static dyn interface::All {
     bsp::console::console()
 }
--- rusty_berry_os/src/main.rs
+++ rusty_berry_os/src/main.rs
@@ -3,6 +3,7 @@
 #![feature(asm_const)]
 #![feature(format_args_nl)]
 #![feature(panic_info_message)]
+#![feature(trait_alias)]
 #![no_main]
 #![no_std]
 
@@ -11,9 +12,18 @@ mod console;
 mod cpu;
 mod panic_wait;
 mod print;
+mod synchronization;
 
 unsafe fn kernel_init() -> ! {
-    println!("Hello from Rust!");
+    use console::console;
 
-    panic!("Stopping here.")
+    println!("[0] Booting on: {}", bsp::board_name());
+
+    println!("[1] Hello world from the kernel!");
+
+    println!("[2] Chars written: {}", console().chars_written());
+
+    println!("[3] Stopping here.");
+
+    cpu::wait_forever()
 }
