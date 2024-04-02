3178ce3428b101166eef26737559080f4f21d38f
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-04-02 09:02:10 +0200

    Add new code to bsp

--- rusty_berry_os/src/bsp.rs
+++ rusty_berry_os/src/bsp.rs
@@ -6,3 +6,15 @@ mod raspberrypi;
 #[cfg(any(feature = "bsp_rpi3"))]
 #[allow(unused_imports)]
 pub use raspberrypi::*;
+
+pub fn board_name() -> &'static str {
+    #[cfg(feature = "bsp_rpi3")]
+    {
+        "Raspberry Pi 3"
+    }
+
+    #[cfg(not(feature = "bsp_rpi3"))]
+    {
+        "Unknown board"
+    }
+}
