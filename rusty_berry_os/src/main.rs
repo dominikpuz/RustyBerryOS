ad5b7c6a3e022ab6763cde92ba9b43a6f3b5d90f
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-05-28 08:39:55 +0200

    Implement basic 64KB virtual memory layout

--- rusty_berry_os/src/main.rs
+++ rusty_berry_os/src/main.rs
@@ -1,9 +1,22 @@
-//! The `kernel` binary.
+//! From a namespace perspective, **memory** subsystem code lives in:
+//!
+//! - `crate::memory::*`
+//! - `crate::bsp::memory::*`
+//!
+//! # Boot flow
+//!
+//! 1. The kernel's entry point is the function `cpu::boot::arch_boot::_start()`.
+//!     - It is implemented in `src/_arch/__arch_name__/cpu/boot.s`.
+//! 2. Once finished with architectural setup, the arch code calls `kernel_init()`.
 
 #![allow(clippy::upper_case_acronyms)]
+#![allow(incomplete_features)]
+#![allow(internal_features)]
 #![feature(asm_const)]
 #![feature(const_option)]
+#![feature(core_intrinsics)]
 #![feature(format_args_nl)]
+#![feature(int_roundings)]
 #![feature(panic_info_message)]
 #![feature(trait_alias)]
 #![feature(unchecked_math)]
@@ -11,16 +24,33 @@
 #![no_std]
 
 mod bsp;
+mod common;
+mod console;
 mod cpu;
+mod driver;
+mod exception;
+mod memory;
 mod panic_wait;
 mod print;
 mod synchronization;
-mod driver;
-mod console;
 mod time;
-mod exception;
 
+/// Early init code.
+///
+/// # Safety
+///
+/// - Only a single core must be active and running this function.
+/// - The init calls in this function must appear in the correct order:
+///     - MMU + Data caching must be activated at the earliest. Without it, any atomic operations,
+///       e.g. the yet-to-be-introduced spinlocks in the device drivers (which currently employ
+///       NullLocks instead of spinlocks), will fail to work (properly) on the RPi SoCs.
 unsafe fn kernel_init() -> ! {
+    use memory::mmu::interface::MMU;
+
+    if let Err(string) = memory::mmu::mmu().enable_mmu_and_caching() {
+        panic!("MMU: {}", string);
+    }
+
     // Initialize the BSP driver subsystem.
     if let Err(x) = bsp::driver::init() {
         panic!("Error initializing BSP driver subsystem: {}", x);
@@ -34,10 +64,9 @@ unsafe fn kernel_init() -> ! {
     kernel_main()
 }
 
-
-/// The main function running after init.
+/// The main function running after the early init.
 fn kernel_main() -> ! {
-    use console::console;
+    use console::{console, interface::Write};
     use core::time::Duration;
 
     info!(
@@ -46,6 +75,9 @@ fn kernel_main() -> ! {
         env!("CARGO_PKG_VERSION")
     );
     info!("Booting on: {}", bsp::board_name());
+
+    info!("MMU online. Special regions:");
+    bsp::memory::mmu::virt_mem_layout().print_layout();
 
     let (_, privilege_level) = exception::current_privilege_level();
     info!("Current privilege level: {}", privilege_level);
@@ -63,6 +95,13 @@ fn kernel_main() -> ! {
 
     info!("Timer test, spinning for 1 second");
     time::time_manager().spin_for(Duration::from_secs(1));
+
+    let remapped_uart = unsafe { bsp::device_driver::PL011Uart::new(0x1FFF_1000) };
+    writeln!(
+        remapped_uart,
+        "[     !!!    ] Writing through the remapped UART at 0x1FFF_1000"
+    )
+        .unwrap();
 
     info!("Echoing input now");
 
